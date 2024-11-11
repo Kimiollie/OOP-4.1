34f5044b3424d4bf5c4d24dabf7b096b1ef5f6a9
Trung_Doan <44894608+viettrung2103@users.noreply.github.com>
2024-11-11 14:12:49 +0200

    Create Trung's branch, edit checkPalindrome method, adding comment to show Palindrome definition and

--- src/PalindromeChecker.java
+++ src/PalindromeChecker.java
@@ -2,15 +2,21 @@ import java.util.Scanner;
 
 public class PalindromeChecker {
 
+    /*
+    a word, phrase, or sequence that reads the same backwards as forwards, e.g. madam or nurses run.
+     */
+
     public static void main(String[] args) {
 
+        // instantiate a scanner which take input from keyboard
         Scanner scanner = new Scanner(System.in);
+        // the scanner read from the begining of the sentence until reading character "\n" = user enter "Enter"
         String input = scanner.nextLine();
 
         String transformed = transformInput(input);
 
-        boolean isPalindrome = checkPalindrome(transformed);
-        if (isPalindrome) {
+//        boolean isPalindrome = checkPalindrome(transformed);
+        if (checkPalindrome((transformed))) {
             System.out.println("The transformed input is a palindrome.");
         } else {
             System.out.println("The transformed input is not a palindrome.");
@@ -18,19 +24,18 @@ public class PalindromeChecker {
     }
 
     public static String transformInput(String input) {
+        //turn the input String to lowerCased String
         input = input.toLowerCase();
+        // remove all ',' inside ths String
         input = input.replace(",", "");
+        // remove all '!' inside the String
         input = input.replace("!", "");
         return input;
     }
 
     public static boolean checkPalindrome(String input) {
         String reversed = reverseString(input);
-        if (input.equals(reversed)) {
-            return true;
-        } else {
-            return false;
-        }
+        return input.equals(reversed);
     }
 
     public static String reverseString(String input) {
